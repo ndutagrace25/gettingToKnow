faa7f1fb74917c09932cc2da59f47c68265b0b01
imhilla <hillaryodhiambo@gmail.com>
2021-09-30 11:45:43 +0300

    added promises

---
+++ promises.js
@@ -0,0 +1,40 @@
+const posts = [{ title: "Post one", body: "This is post one" }, { title: "Post two", body: "This is post two" }]
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`
+    })
+    document.body.innerHTML = output;
+  }, 1000)
+}
+
+function createPosts(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve()
+      } else {
+        reject('Error:  something went wrong')
+      }
+    }, 2000)
+  })
+}
+
+// createPosts({ title: "Post three", body: "This is post three" }).then(() => {
+//   getPosts()
+// });
+
+// Promise.all
+const promise1 = Promise.resolve('Hello world');
+const promise2 = 12;
+const promise3 = new Promise((resolve, reject) => 
+  setTimeout(resolve, 2000, 'Goodbye')
+)
+
+Promise.all([promise1, promise2, promise3]).then(values => console.log(values))
